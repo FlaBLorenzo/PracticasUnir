2996c5bd0998d5de1d7598a7ec882ae120ab69c8
FlaBLorenzo <flalorenzomulatero@gmail.com>
2024-12-12 14:10:31 +0100

    Update microservicio.py

--- app/microservicio.py
+++ app/microservicio.py
@@ -4,24 +4,24 @@ app = Flask(__name__)
 
 @app.route('/calc/add/<int:a>/<int:b>', methods=['GET'])
 def add(a, b):
-    result = a + b
+    result = {"result": a + b}
     return jsonify(result=result), 200
 
 @app.route('/calc/subtract/<int:a>/<int:b>', methods=['GET'])
 def subtract(a, b):
-    result = a - b
+    result = {"result": a - b}
     return jsonify(result=result), 200
 
 @app.route('/calc/multiply/<int:a>/<int:b>', methods=['GET'])
 def multiply(a, b):
-    result = a * b
+    result = {"result": a * b}
     return jsonify(result=result), 200
 
 @app.route('/calc/divide/<int:a>/<int:b>', methods=['GET'])
 def divide(a, b):
     if b == 0:
-        abort(406, description="Division by zero is not allowed")
-    result = a / b
+        return "Division by zero is not allowed", 406
+    result = {"result": a / b}
     return jsonify(result=result), 200
 
 if __name__ == "__main__":
