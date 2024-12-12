e91c3b038c5c1c041b9a5008b0c4cfcaa02afe11
FlaBLorenzo <flalorenzomulatero@gmail.com>
2024-12-12 16:06:31 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -1,7 +1,8 @@
 import unittest
 from urllib.request import urlopen
-from urllib.error import URLError, HTTPError
+from urllib.error import HTTPError
 import http.client
+import json
 
 BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
@@ -11,28 +12,43 @@ class TestApi(unittest.TestCase):
 
     def test_api_add(self):
         url = f"{BASE_URL}/calc/add/1/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
-        self.assertEqual(response.read().decode().strip(), '{"result": 3}', "ERROR ADD")
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
+            result = json.loads(response.read().decode().strip())
+            self.assertEqual(result, {"result": 3}, "ERROR ADD")
+        except HTTPError as e:
+            self.fail(f"HTTPError: {e}")
 
     def test_api_divide(self):
         url = f"{BASE_URL}/calc/divide/10/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
-        self.assertEqual(response.read().decode().strip(), '{"result": 5.0}', "ERROR DIVIDE")
-
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
+            result = json.loads(response.read().decode().strip())
+            self.assertEqual(result, {"result": 5.0}, "ERROR DIVIDE")
+        except HTTPError as e:
+            self.fail(f"HTTPError: {e}")
 
     def test_api_multiply(self):
         url = f"{BASE_URL}/calc/multiply/3/4"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
-        self.assertEqual(response.read().decode().strip(), '{"result": 12}', "ERROR MULTIPLY")
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
+            result = json.loads(response.read().decode().strip())
+            self.assertEqual(result, {"result": 12}, "ERROR MULTIPLY")
+        except HTTPError as e:
+            self.fail(f"HTTPError: {e}")
 
     def test_api_subtract(self):
         url = f"{BASE_URL}/calc/subtract/5/3"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
-        self.assertEqual(response.read().decode().strip(), '{"result": 2}', "ERROR SUBTRACT")
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
+            result = json.loads(response.read().decode().strip())
+            self.assertEqual(result, {"result": 2}, "ERROR SUBTRACT")
+        except HTTPError as e:
+            self.fail(f"HTTPError: {e}")
 
 if __name__ == '__main__':
     unittest.main()
